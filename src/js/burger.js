e92ef7b6dd23a7ea810b36b49530fe14781eaec4
Salamandrachka <oleksandrivnasuper@gmail.com>
2023-11-23 20:08:23 -0500

    jquery menu fixed

--- src/js/burger.js
+++ src/js/burger.js
@@ -21,7 +21,9 @@
 let $menuBtn = $(".hamburger");
 let $body = $(".body");
 let $menu = $(".nav-mobile__wrapper");
-let $menuItem = $(".nav-mobile__link");
+let $menuItem = $(
+  ".nav-mobile__link, .mobile-dropdown__link, .mobile-dropdown-sub__link"
+);
 
 $menuBtn.on("click", function () {
   $menuBtn.toggleClass("active");
@@ -33,7 +35,7 @@ $menuItem.on("click", function () {
   $menuBtn.toggleClass("active");
   $menu.toggleClass("active");
   $body.toggleClass("menu-active");
-});
+});   
 
 // Додаємо клас "active-menu" при наведенні
 $(".nav-menu__item").hover(
@@ -52,7 +54,7 @@ $(".nav-mobile__item").hover(
   },
   function () {
     $(this)
-      .find(".mobile-dropdown__wrapper").removeClass("active-menu-mobile ");
+      .find(".mobile-dropdown__wrapper").removeClass("active-menu-mobile");
   }
 );
 
